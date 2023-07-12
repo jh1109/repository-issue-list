53d14f6ec2f76914a41e89bd6883b5b3e6d28b64
juhee <nohjuhee227@gmail.com>
2023-07-13 05:54:10 +0900

    Refac: context API 활용하여 REST API 연동

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { HttpClient } from './httpClient/httpClient';
+import { IssueProvider } from './store/issueContext';
+import { IssueService } from './services/IssueService';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const httpClient = new HttpClient('https://api.github.com/');
+const issueService = new IssueService(httpClient);
+
 root.render(
-  <>
+  <IssueProvider issueService={issueService}>
     <App />
-  </>
+  </IssueProvider>
 );
