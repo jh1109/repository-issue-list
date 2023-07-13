56a30dec430c947bd00f87e9d3d0ec2d9b6e148f
juhee <nohjuhee227@gmail.com>
2023-07-13 19:59:33 +0900

    Feat: IssueDetailPage 라우터 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import GlobalStyle from './GlobalStyle';
+import IssueDetailPage from './pages/IssueDetailPage';
 
-const router = createBrowserRouter([{ path: '/', element: <HomePage /> }]);
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/issues', element: <IssueDetailPage /> },
+]);
 
 function App() {
   return (
---
+++ src/pages/IssueDetailPage.tsx
@@ -0,0 +1,14 @@
+import Header from '../components/layout/Header';
+
+const IssueDetailPage = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <p>issue 상세 페이지</p>
+      </main>
+    </>
+  );
+};
+
+export default IssueDetailPage;
